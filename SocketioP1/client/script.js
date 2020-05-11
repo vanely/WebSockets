618b962de7290a98696b7b7d3339ff04c3b90d91
vanely <vanelys94@gmail.com>
2020-05-10 21:59:02 -0400

    rendering socket data

--- SocketioP1/client/script.js
+++ SocketioP1/client/script.js
@@ -1,4 +1,10 @@
 const socket = io("http://localhost:5050");
+const messageBox = document.querySelector(".message-box");
+const inputMessage = document.querySelector(".message-input");
+const submitBtn = document.querySelector(".message-submit");
+
+const showData = document.createElement("section");
+
 
 // initiate socket connection to specified server
 socket.on("connect", (data) => {
@@ -7,9 +13,17 @@ socket.on("connect", (data) => {
   socket.on("welcome", (message) => { 
     console.log("Message from server", message);
   });
-
+  
   // emit is again used to send data via an object to the server
   // the emit() event string. In our case "message" can be any string, but has to match the on() event string on the server side
   socket.emit('message', {data: "Hello from the client side of things!"});
 })
-console.log(socket)
+
+
+
+socket.on("sendies", (msg) => {
+  showData.textContent = msg.things;
+  messageBox.appendChild(showData);
+  console.log("More server messages: ", msg);
+  console.log(socket)
+})
--- SocketioP1/server/server.js
+++ SocketioP1/server/server.js
@@ -10,13 +10,15 @@ const io = socketio(server)
 io.on("connection", (socket, req) => {
   // emit sends a message from the server.
   // the emit() event string. In our case "welcome", can be any string, but has to match the on() event string on the client side.
-  socket.emit("welcome", {data: "Welcome to the 'WebSocket Server'"});
+  socket.emit("welcome", {data: "Connection successful. Welcome to the 'WebSocket Server'"});
 
   // message event listens for data coming from the client.
   // the on() listening event name. In this case "message" can be any string, but has to match the emit() event string on the client side .
   socket.on("message", (msg) => {
     console.log("Incoming socket message: ", msg)
   })
+
+  socket.emit("sendies",{things: "lkahdsjklfha"});
 });
 
 console.log(io);
