af628d31fafa3659ab63c60d7ac260a81d4db436
vanely <vanelys94@gmail.com>
2020-05-29 09:04:43 -0400

    checking ping latency

--- SocketioP1/chat-client-test/server.js
+++ SocketioP1/chat-client-test/server.js
@@ -10,12 +10,12 @@ app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(port);
 // make the socket server listen to the http server, and set it as our instance of "io". This instantiation can also take a config object
 // refer to server docs for options for config https://socket.io/docs/server-api/
-const io = socketio(expressServer, {
+const io = socketio(expressServer/*, {
   path: "/socket.io", // if we look at the html page this is the entry path for the socket.io.js module(can be set to anything)
   serveClient: true, // toggles whether to serve client files or not(our html).
   origins: "*", // origins that are allowed to make a connection to this server(default is any and all).
   wsEngine: "ws", // choose what web socket engine to use. Like a C++ implementation if that's what was being used
-});
+}*/);
 
 // listen for "connection". NOTE: this is socket.io specific. listens for connection requests to this server.
 // all socket.io connections are open on a namespace. This can be an explicitly specified namespace, or default such as this case. our main connection.
